73f1d33e7d51afc9c15bd7050aa9a21bc347fd9a
Loblolly <loblolly690@gmail.com>
2022-03-01 14:27:41 -0500

    Update input styling

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 
 type Props = {
   setAddress: (address: string) => void;
@@ -11,20 +11,30 @@ export function UserInput({ setAddress }: Props) {
         setAddress(event.currentTarget["address"].value);
         event.preventDefault();
       }}
-      style={{ display: "flex", gap: "15px", justifyContent: "center" }}
     >
-      <TextField
-        label="Address"
-        type="text"
-        name="address"
-        autoFocus={true}
-        defaultValue={process.env.REACT_APP_DEFAULT_ADDRESS || ""}
-        variant="standard"
-        style={{ minWidth: "375px" }}
-      />
-      <Button type="submit" variant="contained">
-        Query
-      </Button>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+      >
+        <Grid item xs={12} sm={8}>
+          <TextField
+            fullWidth
+            label="Address"
+            typeof="text"
+            name="address"
+            autoFocus={true}
+            defaultValue={process.env.REACT_APP_DEFAULT_ADDRESS || ""}
+          />
+        </Grid>
+        <Grid item xs={12} sm={1}>
+          <Button type="submit" variant="contained" fullWidth>
+            Query
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 }
